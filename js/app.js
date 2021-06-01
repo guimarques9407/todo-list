dabc0792a107c255d7d81eabf13c068437454997
Guilherme Marques <guim_santos@yahoo.com.br>
2021-06-01 15:05:18 -0300

    uptadte script

--- js/app.js
+++ js/app.js
@@ -9,20 +9,30 @@ function addItemToUi(e){
     if(itemInput.value.trim()==""||itemInput.value==""){
         feedback.style.display="flex";
     }
-    //ver se da para passar a div item como this para todos os eventListeners
     else{
-        itemContainer.insertAdjacentHTML("beforeend",`<div class="item my-3" onclick="checkItem(this, event)">
+        itemContainer.insertAdjacentHTML("beforeend",`<div class="item my-3" onclick="chooseButton(this, event)">
             <h5 class="item-name text-capitalize">${itemInput.value}</h5>
             <div class="item-icons">
             <a href="#" class="complete-item mx-2 item-icon" title="checar item" ><i class="far fa-check-circle"></i></a>
             <a href="#" class="edit-item mx-2 item-icon" title="editar item"><i class="far fa-edit"></i></a>
             <a href="#" class="delete-item item-icon" title="excuir item"><i class="far fa-times-circle"></i></a>
-            </div>`)};
+            </div>`)
+            itemInput.value=""
+        };
+
 }
 
- function checkItem(element,e){
+ function chooseButton(element,e){
+    let itemName=element.querySelector("h5")
     if(e.target.classList.contains("fa-check-circle")){
-        element.querySelector("h5").classList.add("completed");
+        itemName.classList.toggle("completed");
+    }
+    else if(e.target.classList.contains("fa-edit")){
+        itemInput.value=itemName.textContent;
+        itemContainer.removeChild(element);
+    }
+    else if(e.target.classList.contains("fa-times-circle")){
+        itemContainer.removeChild(element);
     }
  }
 
